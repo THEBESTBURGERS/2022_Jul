5e9b6fc37d9cc866d0ef8e9673d1d372ac865682
chestnut1044 <wbk1620@gmail.com>
2022-07-28 23:28:09 +0900

    Learn : Ajax

---
+++ 0728/43-04.js
@@ -0,0 +1,14 @@
+const obj = {
+  name: 'Lee',
+  age: 20,
+  alive: true,
+  hobby: ['traveling', 'tennis']
+};
+
+// 객체를 JSON 포맷의 문자열로 변환한다.
+const json = JSON.stringify(obj, null, 2);
+console.log(typeof json, json);
+// JSON 포맷의 문자열을 객체로 변환한다.
+const parsed = JSON.parse(json);
+console.log(typeof parsed, parsed);
+// object {name: "Lee", age: 20, alive: true, hobby: ["traveling", "tennis"]}
